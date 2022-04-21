6449b87288fd49a96234296999cf63385fb940ce
TitouanLarnicol <larnicol.titouan@orange.fr>
2022-04-21 16:08:03 +0200

    Add pagination + click on event name

--- extract-runners.js
+++ extract-runners.js
@@ -14,7 +14,7 @@ const getItraFromRunner = (lastName, firstName) => {
         redirect: 'follow'
     };
 
-    return fetch("https://itra.run/api/runner/find", requestOptions)
+    return fetch(ITRA_URL, requestOptions)
         .then(response => response.json())
         .catch(error => console.log('error', error));
 }
@@ -24,7 +24,7 @@ const getItraFromRunner = (lastName, firstName) => {
 const URL = "https://inscriptions-l-chrono.com/trailnivoletrevard2022/registrations-list";
 const ITRA_URL = "https://itra.run/api/runner/find";
 
-const fetchAndAnalyzeRunners = (page) => {
+async function fetchAndAnalyzeRunners(page) {
     const runners = await page.evaluate(() => {
         const tds = Array.from(document.querySelectorAll('table tr td:nth-child(-n + 2)'))
         return tds.map(p => p.innerText).reduce((acc, val, idx) =>
@@ -33,19 +33,25 @@ const fetchAndAnalyzeRunners = (page) => {
                 : (acc ? `${acc},${val}` : `${val}`), '').split(',')
     });
 
-    for (let runner of runners) {
-        try {
-            const [lastName, firstName] = runner.split(' ');
-            const data = await getItraFromRunner(lastName, firstName);
-            if (data.results.length > 0) {
-                const runnerData = data.results[0];
-                console.log("Runner " + runnerData.firstName + " " + runnerData.lastName + " with ITRA " + runnerData.pi)
-            }
-        }
-        catch (err) {
-            console.log(err)
-        }
-    }
+    const results = await Promise.all(runners.map(runner => {
+        const [lastName, firstName] = runner.split(' ');
+        return getItraFromRunner(lastName, firstName)
+    }));
+    // for (let runner of runners) {
+    //     try {
+    //         const [lastName, firstName] = runner.split(' ');
+    //         const data = await getItraFromRunner(lastName, firstName);
+    //         if (data.results.length > 0) {
+    //             const runnerData = data.results[0];
+    //             results.push({ lastName, firstName, itra: runnerData.pi });
+    //             console.log("Runner " + runnerData.firstName + " " + runnerData.lastName + " with ITRA " + runnerData.pi)
+    //         }
+    //     }
+    //     catch (err) {
+    //         console.log(err)
+    //     }
+    // }
+    return results;
 }
 
 // invoking the main function
@@ -55,8 +61,28 @@ function _init() {
         const page = await browser.newPage();
         await page.goto(URL);
 
-        fetchAndAnalyzeRunners(page);
+        let optionValue = await page.$$eval('option', options => options.find(o => o.innerText === "Le Malpassant")?.value)
+        await page.select('select#competitions', optionValue);
+        await page.keyboard.press('Enter');
 
+        var results = []; // variable to hold collection of all book titles and prices
+        let lastPage;
+        // defined simple loop to iterate over number of catalogue pages
+        while (!lastPage) {
+            // wait 1 sec for page load
+            await page.waitFor(2000);
+
+            const tmp = await fetchAndAnalyzeRunners(page);
+            results = results.concat(...tmp);
+
+            lastPage = await page.evaluate(() => {
+                return document.querySelector('li.next-page.disabled');
+            });
+            if (!lastPage) {
+                await page.click('li.next-page > a');
+            }
+
+        }
         await browser.close();
     })();
 }
