bd82f72a30d98676c1ccd33c3f21a55073ef0a7f
TitouanLarnicol <larnicol.titouan@orange.fr>
2022-05-02 14:10:35 +0200

    Create sqlite db

--- public/db/connect.js
+++ public/db/connect.js
@@ -1,5 +1,20 @@
+const sqlite3 = require("sqlite3")
+
+const db = new sqlite3.Database('db.sqlite');
+
 function _initSQL() {
-    // CONNECT TO SQLITE
+    db.serialize(function () {
+        // CREATE TABLES MANY TO MANY
+        // RUNNERS, RACES, ASSIGNEES (join)
+        db.run(`CREATE TABLE IF NOT EXISTS runners (
+            runner_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT,
+            last_name TEXT,            
+        )`, (err, row) => {
+            if (err) { console.log('err', err) }
+            else { console.log('row', row) }
+        });
+    });
 }
 
 module.exports = {
